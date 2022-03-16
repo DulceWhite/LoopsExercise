f7f93a5e08220607c68811e642f7c871b279f3cf
Dulce <dulce.a.suarez@gmail.com>
2022-03-16 09:42:24 -0500

    After review commit.

--- script.js
+++ script.js
@@ -4,14 +4,14 @@
 
 for (i=1; i<=7; i++){
     console.log(i);
-}
+};
 
 console.log(`-.-.-.-.2.-.-.-.-.-.`);
 //2. 
 
 for (i = 5; i <= 24; i += 4){
     console.log(i);
-}
+};
 
 //3a
 const wizards = [
@@ -24,7 +24,7 @@ console.log(`-.-.-.-.3b.-.-.-.-.-.`);
 //3b
 for (identity of wizards){
     console.log(identity);
-}
+};
 
 
 //4a
@@ -32,14 +32,16 @@ let harryPotterMovies = 0;
 
 console.log(`-.-.-.-.4b.-.-.-.-.-.`);
 //4b
-while(harryPotterMovies < 9){
-    console.log(harryPotterMovies);
+while(harryPotterMovies < 8){
+    console.log(harryPotterMovies);//1-7
     harryPotterMovies ++;
-}
+};
+
+
 
 console.log(`-.-.-.-.4c.-.-.-.-.-.`);
 //4c
-console.log(harryPotterMovies -= 1);
+console.log(harryPotterMovies);//8
 
 //BONUS
 
@@ -57,7 +59,10 @@ for(house of hogwartsHouses){
     for(houseName of house){
         console.log(houseName);
     }
+    //this will make a divider between each house name
+    console.log(`.-.--.-.-.-.-.-.-.--.-`);
 }
+
 
 //6a
 const quote = [
@@ -70,33 +75,50 @@ const quote = [
 console.log(`-.-.-.-.BONUS 6b.-.-.-.-.-.`);
 
 //6b.
-console.log(...quote);
-
-//or
+// console.log(...quote);
 
 
-//or
-//const newVar = [...quote];
-//console.log(...newVar);
 
-//this one
+// let hagrid = quote.join(` `);
+// console.log(hagrid);
 
 
-for (big of quote){
-    console.log(big);
+
+for (all=0; all<=0; all++){
+    console.log(quote.join(` `));
 }
 
+//variable to store the values
+let magic = ``;
+for(i=0; i< quote.length; i++){
+    magic += ` ${quote[i]}`//storing and adding the values
+}
+magic = magic.trim;//getting rid of empty spaces at beginning and end
+console.log(magic);
+
+// var all =[];
+// for(i=0; i<=quote.length; i++){
+//     all = all + [i];
+// }
+// console.log(all);//01234
+//almost got it, jsut need it to store the values in a variable like 
 
 
 //7
+
+//Inspired by FizzBuzz
 console.log(`-.-.-.-.BONUS 7.-.-.-.-.-.`);
 for (i=1; i<=25; i++ ) {
-    console.log(i);
-    if(i% 3 ===0){
-        console.log(`Expecto`);
-    } else if (i% 5 ===0){
-        console.log(`Patronum`);
-    } else if(i%3 ===0 && i%5===0){
+    if(i%3 ===0 && i%5===0){
         console.log(`Expecto Patronum`);
+    } else if(i% 3 ===0){
+    console.log(`Expecto`);
+     
+    } else if(i% 5 ===0){
+        console.log(`Patronum`);
+    } else {
+        console.log(i);
     }
 }
+
+
